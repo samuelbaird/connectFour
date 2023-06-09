bb7fbbf9db0bb7802bd12f3d8e5ac1f81158b744
Samuel Baird <sam@samuelbaird.com>
2023-06-09 14:46:08 -0300

    beginning to build the JS

--- js/main.js
+++ js/main.js
@@ -3,6 +3,19 @@
 
 /*----- state variables -----*/
 
+// - turn: 1/-1
+// - board: 2D Array
+//     null -> no player
+//     1/-1 -> player at that cell
+// - winner
+//     null -> no winner/in play
+//     1/-1 -> winner
+//     'T' - >tie
+
+let board;
+let turn;
+let winner;
+
 
 /*----- cached elements  -----*/
 
@@ -11,3 +24,22 @@
 
 
 /*----- functions -----*/
+
+init();
+function init() {
+    board = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+    ]
+    turn = 1;
+    winner = null;
+render();
+}
+function render() {
+    
+}
